443675ee9bbaec13c4ac92293fbfb4859732a0c2
dzejkop <mrkuka403@gmail.com>
2023-11-11 22:46:30 +0100

    mqtt uloha

--- mqtt_du/app_main.c
+++ mqtt_du/app_main.c
@@ -1,12 +1,3 @@
-/* MQTT (over TCP) Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -31,8 +22,84 @@
 #include "mqtt_client.h"
 #include "driver/gpio.h"
 
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+
 static const char *TAG = "MQTT_EXAMPLE";
-bool state = false;
+bool led_state = false;
+
+bool button_pressed = false;
+esp_mqtt_client_handle_t client;
+adc_oneshot_unit_handle_t adc1_handle;
+
+
+void board_config()
+{
+    gpio_reset_pin(GPIO_NUM_5);
+    gpio_set_direction(GPIO_NUM_5, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(GPIO_NUM_5, GPIO_PULLUP_ONLY);
+}
+
+void check_button()
+{
+    while(1)
+    {
+        if(!gpio_get_level(GPIO_NUM_5))
+        {
+            if(!button_pressed)
+            {
+                if(led_state == true)
+                {
+                    led_state = false;
+                    esp_mqtt_client_publish(client, "/marioDzejkop/smart/light/onOff", "0", 0, 0, 1);
+                }
+                else 
+                {
+                    led_state = true;
+                    esp_mqtt_client_publish(client, "/marioDzejkop/smart/light/onOff", "1", 0, 0, 1);
+                }
+                button_pressed = true;
+            }
+        }
+        else button_pressed = false;
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }   
+}
+
+char *get_val(int out, int max_out)
+{
+    if(out == max_out) return "8";
+    else if (out >= (max_out / 8) * 7) return "7";
+    else if(out >= (max_out / 8) * 6) return "6";
+    else if(out >= (max_out / 8) * 5) return "5";
+    else if(out >= (max_out / 8) * 4) return "4";
+    else if(out >= (max_out / 8) * 3) return "3";
+    else if(out >= (max_out / 8) * 2) return "2";
+    else if(out >= max_out / 8) return "1";
+    else return "0";
+}
+
+int divide(int num, int max)
+{
+    return (num + 1) / (max/ 8);
+}
+
+void check_pot()
+{
+    int led_prog = 0;
+    int prev_led_prog = 0;
+    while(1)
+    {
+        prev_led_prog = led_prog;
+        adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &led_prog);
+        if(divide(led_prog,512) != divide(prev_led_prog,512))
+        {
+            esp_mqtt_client_publish(client, "/marioDzejkop/smart/light/brightness", get_val(divide(led_prog,512), 8), 0, 0, 1);
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
 
 static void log_error_if_nonzero(const char *message, int error_code)
 {
@@ -41,29 +108,18 @@ static void log_error_if_nonzero(const char *message, int error_code)
     }
 }
 
-/*
- * @brief Event handler registered to receive MQTT events
- *
- *  This function is called by the MQTT client event loop.
- *
- * @param handler_args user data registered to the event.
- * @param base Event base for the handler(always MQTT Base in this example).
- * @param event_id The id for the received event.
- * @param event_data The data for the event, esp_mqtt_event_handle_t.
- */
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
     esp_mqtt_event_handle_t event = event_data;
-    esp_mqtt_client_handle_t client = event->client;
+    client = event->client;
     int msg_id;
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_publish(client, "/dzejkop/test", "data_dzejkop", 0, 1, 1);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/mario/test", 1);
+        msg_id = esp_mqtt_client_subscribe(client, "/marioDzejkop/smart/light/brightness", 1);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        msg_id = esp_mqtt_client_subscribe(client, "/marioDzejkop/smart/light/onOff", 1);
         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
     case MQTT_EVENT_DISCONNECTED:
@@ -72,8 +128,6 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 
     case MQTT_EVENT_SUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
@@ -83,22 +137,15 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         break;
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        if(strncmp("/mario/test", event->topic, event->topic_len) == 0)
+        if(strncmp("/marioDzejkop/smart/light/onOff", event->topic, event->topic_len) == 0)
         {
-            if(strncmp("on",event->data,event->data_len) == 0)
+            if(strncmp("0",event->data,event->data_len) == 0)
             {
-                gpio_set_level(GPIO_NUM_4, 1);
-                state = true;
+                led_state = false;
             }
-            else if(strncmp("off",event->data,event->data_len) == 0)
+            else if(strncmp("1",event->data,event->data_len) == 0)
             {
-                gpio_set_level(GPIO_NUM_4, 0);
-                state = false;
-            }
-            else if(strncmp("toggle",event->data,event->data_len) == 0)
-            {
-                state = !state;
-                gpio_set_level(GPIO_NUM_4, state);
+                led_state = true;
             }
         }
         
@@ -125,17 +172,24 @@ static void mqtt_app_start(void)
         .broker.address.uri = CONFIG_BROKER_URL,
     };
 
-
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
 }
 
 void app_main(void)
 {
-    gpio_reset_pin(GPIO_NUM_4);
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_9,
+        .atten = ADC_ATTEN_DB_11,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle,ADC_CHANNEL_0, &config));
+   
     ESP_LOGI(TAG, "[APP] Startup..");
     ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
     ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
@@ -152,11 +206,12 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
+    board_config();
 
+    ESP_ERROR_CHECK(example_connect());
+    
     mqtt_app_start();
+
+    xTaskCreate(check_button, "button_task", 2048, NULL, 10, NULL);
+    xTaskCreatePinnedToCore(check_pot, "pot_task", 2048, NULL, 10, NULL, 1);
 }
